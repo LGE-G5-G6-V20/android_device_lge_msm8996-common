f3fb8ae02e1836e15bd1f0f17c465de03a1e9b57
xxseva44 <dev.xxseva44@gmail.com>
2023-04-11 23:20:20 -0400

    Camera: Build display service

--- msm8996.mk
+++ msm8996.mk
@@ -93,6 +93,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
