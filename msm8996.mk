6b7cd5b7e36a58e44bbce1c46815e5a6db239c0e
jmpfbmx <jmpf.bmx@gmail.com>
2024-04-03 22:31:09 +0200

    msm8996-common: Drop IMS
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I6be9ddb091d85fa06ca3e41cf3dbce77fff55b20

--- msm8996.mk
+++ msm8996.mk
@@ -346,8 +346,6 @@ PRODUCT_PACKAGES += \
     libxml2
 
 PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
